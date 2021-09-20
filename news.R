6dbfa131c55f53628b7c0d209e8c5a3cfc897d33
TemTee <temmyteeliz@gmail.com>
2021-09-20 18:32:31 +0100

    cleaning the data

--- news.R
+++ news.R
@@ -4,11 +4,13 @@ library(tidyr)
 library(stringi)
 library(tm)
 library(SnowballC)
+library(wordcloud)
+library(caret)
 true_news <- read_csv("~/R/datasets/Fake and True News/True.csv") %>% 
   mutate(type = factor("fake"))
 fake_news <- read_csv("~/R/datasets/Fake and True News/Fake.csv") %>% 
   mutate(type = factor("true"))
-news <- sample_n(full_join(fake_news,true_news), 44898) %>% 
+news <- sample_n(left_join(fake_news,true_news), 44898) %>% 
   select(-title, -subject, -date)
 news
 table(news$type)
@@ -20,5 +22,20 @@ replacepunt <- function(p){
 news_corpus_clean <- DocumentTermMatrix(news_corpus, 
                                         control = list(
                                           tolower = TRUE,
-                                          r
+                                          removeNumbers = TRUE,
+                                          stopwords = TRUE,
+                                          replacepunt = TRUE,
+                                          stemming = TRUE
                                         ))
+news_corpus_clean
+
+
+# ***************      Build Train and test set 80-20%   ******************
+
+
+train_set <- news_corpus_clean[1:35911, ]
+train_label <- news[1:35911, ]$type
+test_set <- news_corpus_clean[35912:44898, ]
+test_label <- news[35912:44898, ]$type
+
+prop.table(table(train_label))
